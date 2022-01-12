17ad665b7a29bb88066358a73ee6e0be661cd1de
antipachita <nice.strogonov@inbox.ru>
2022-01-13 00:35:08 +0300

    feat: new code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
-  
+    let a = String(n);
+    let b = Array.from(a);
+      let w = [];
+      
+    for (let i = 0; i<a.length; i++) {
+      w.unshift(b[i])
+    }
+    
+    if (b[0] === '-') {
+      w = w.slice(0,-1);
+    } 
+      
+    let c = w.toString();
+    let e  = c.replace(/[,]/g, ' ');
+    let f  = e.replace(/\s/g, '');
+    return Number(f);
+    
 }
